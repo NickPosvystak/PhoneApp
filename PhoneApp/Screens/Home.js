dd4c51abdd051815783ee19007a4e0355d6c9cbd
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2024-03-02 18:37:32 +0100

    settings

--- PhoneApp/Screens/Home.js
+++ PhoneApp/Screens/Home.js
@@ -1,13 +1,61 @@
 import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 import Car from "../Images/car.png";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+function Settings() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+function Profile() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Profile!</Text>
+    </View>
+  );
+}
+const Tabs = createBottomTabNavigator();
 
 const Home = () => {
   return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-      <Image source={Car} style={{ width: 350, height: 200 }} />
-    </View>
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Profile") {
+            iconName = focused
+              ? "ios-information-circle"
+              : "ios-information-circle-outline";
+          } else if (route.name === "Settings") {
+            iconName = focused ? "ios-list-box" : "ios-list";
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+        tabBarStyle: [
+          {
+            display: "flex",
+          },
+          null,
+        ],
+      }}
+    >
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+        <Image source={Car} style={{ width: 350, height: 200 }} />
+      </View>
+      <Tabs.Screen name="Settings" component={Settings} />
+      <Tabs.Screen name="Profile" component={Profile} />
+    </Tabs.Navigator>
   );
 };
 
--- PhoneApp/Screens/RegistrationScreen.js
+++ PhoneApp/Screens/RegistrationScreen.js
@@ -12,6 +12,7 @@ import {
   View,
 } from "react-native";
 
+
 export const RegistrationScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
