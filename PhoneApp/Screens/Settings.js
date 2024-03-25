f1fbe40a1666569013f731979d15c2c469fc92e8
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2024-03-25 21:50:17 +0100

    Camera

--- PhoneApp/Screens/Settings.js
+++ PhoneApp/Screens/Settings.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
-
-import { Text, View } from "react-native";
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+import * as Permissions from "expo-permissions";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Settings() {
   const [hasPermission, setHasPermission] = useState(null);
   const [cameraRef, setCameraRef] = useState(null);
-  const { type, setType } = useState(Camera.Constants.Type.back);
+  const [ type, setType ] = useState(Camera.Constants.Type.back);
 
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestPermissionAsync();
-      await MediaLibrary.requestPermissionAsync();
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      await MediaLibrary.requestPermissionsAsync();
 
       setHasPermission(status === "granted");
     })();
@@ -24,8 +26,78 @@ export default function Settings() {
   }
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Settings!</Text>
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type} ref={setCameraRef}>
+        <View style={styles.photoView}>
+          <TouchableOpacity
+            style={styles.flipContainer}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <Text style={{ fontSize: 18, marginBottom: 10, color: "white" }}>
+              {" "}
+              Flip{" "}
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={async () => {
+              if (cameraRef) {
+                const { uri } = await cameraRef.takePictureAsync();
+                await MediaLibrary.createAssetAsync(uri);
+              }
+            }}
+          >
+            <View style={styles.takePhotoOut}>
+              <View style={styles.takePhotoInner}></View>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  camera: { flex: 1 },
+  photoView: {
+    flex: 1,
+    backgroundColor: "transparent",
+    justifyContent: "flex-end",
+  },
+
+  flipContainer: {
+    flex: 0.1,
+    alignSelf: "flex-end",
+  },
+
+  button: { alignSelf: "center" , paddingBottom: 10,},
+
+  takePhotoOut: {
+    borderWidth: 2,
+    borderColor: "white",
+    height: 50,
+    width: 50,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+    
+  },
+
+  takePhotoInner: {
+    borderWidth: 2,
+    borderColor: "white",
+    height: 40,
+    width: 40,
+    backgroundColor: "white",
+    borderRadius: 50,
+  },
+});
